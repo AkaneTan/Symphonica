bd296fae41f16f3b25d68523b7137ff2c7162edd
Shinjo Akane <akane@akanework.org>
2023-05-22 22:34:37 +0800

    Symphonica: Migrate to ksp for Glide
    Signed-off-by: Shinjo Akane <akane@akanework.org>

--- app/src/main/java/org/akanework/symphonica/logic/SymphonicaGlideModule.kt
+++ app/src/main/java/org/akanework/symphonica/logic/SymphonicaGlideModule.kt
@@ -17,8 +17,15 @@
 
 package org.akanework.symphonica.logic
 
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
 @GlideModule
-class SymphonicaGlideModule : AppGlideModule()
+class SymphonicaGlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setLogLevel(Log.ERROR)
+    }
+}
