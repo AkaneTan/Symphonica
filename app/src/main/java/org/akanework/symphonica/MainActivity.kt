2dd2e0b0ca698faf6f3af55fd7efe1db04225745
Shinjo Akane <Akane@Akanework.org>
2023-07-10 03:19:33 +0800

    MainActivity: Delete unused import directive
    Signed-off-by: Shinjo Akane <Akane@Akanework.org>
    
    +# Please enter the commit message for your changes. Lines starting

--- app/src/main/java/org/akanework/symphonica/MainActivity.kt
+++ app/src/main/java/org/akanework/symphonica/MainActivity.kt
@@ -31,7 +31,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
